dcc168830cb0df7143cef53742de6ce648cee51b
AnnaShino <64633651+AnnaShino@users.noreply.github.com>
2021-04-26 19:34:10 +0200

    Update WyborJezyka.kt

--- WyborJezyka.kt
+++ WyborJezyka.kt
@@ -1,5 +1,7 @@
 package com.example.ktg
 
+//wymagane importy związane z działaniem aplikacji
+
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -12,6 +14,7 @@ import kotlin.system.exitProcess
 class WyborJezyka : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         // poniższy fragment pozwala na usunięcie nieestetycznego górnego z nazwą layautu
         super.onCreate(savedInstanceState)
         window.setFeatureInt(
@@ -21,6 +24,7 @@ class WyborJezyka : AppCompatActivity() {
         this.supportActionBar!!.hide()
         setContentView(R.layout.wyborjezyka)
 
+        // przyciski funkcyjne 
         val button1 = findViewById<Button>(R.id.PL)
         button1.setOnClickListener {
             val i = Intent(this, BiometryczneLogowanie::class.java)
@@ -38,6 +42,7 @@ class WyborJezyka : AppCompatActivity() {
 
     }
 
+    //poniższy fragment odpowiada za wychodzenie z aplikacji za pomocą przycisku funkcyjnego
     private var doubleBackToExitPressedOnce = false
     override fun onBackPressed() {
         if (doubleBackToExitPressedOnce) {
